1a0ef8017ce006136e4f3272fd006ba9e6b2e9b7
michael23-lopes <michael23.wellington@gmail.com>
2021-10-23 15:56:00 -0300

    layout form product registration

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const ProductsControllers = require('./app/controllers/ProductsControllers');
 const routes = express.Router();
 
 routes.get('/', function (req, res) {
   return res.render('layout.njk');
+});
+
+routes.get('/products/create', ProductsControllers.create);
+
+routes.get('/ads/create', function (req, res) {
+  return res.rendirect('/products/create');
 });
 
 module.exports = routes;
