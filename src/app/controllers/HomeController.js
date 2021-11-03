8adc69395d13678374198833ab4a95d9c2fa5717
michael23-lopes <michael23.wellington@gmail.com>
2021-11-03 15:23:37 -0300

    configurando a pagina de busca

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,7 +1,6 @@
 const { formatPrice } = require('../../lib/utils');
 
 const Product = require('../models/Product');
-const File = require('../models/File');
 
 module.exports = {
   async index(req, res) {
--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,15 @@ const multer = require('./app/middlewares/multer');
 
 const ProductController = require('./app/controllers/ProductController');
 const HomeController = require('./app/controllers/HomeController');
+const SearchController = require('./app/controllers/SearchController.js');
 
+// Home
 routes.get('/', HomeController.index);
 
+//Search
+routes.get('/products/search', SearchController.index);
+
+// Products
 routes.get('/products/create', ProductController.create);
 routes.get('/products/:id', ProductController.show);
 routes.get('/products/:id/edit', ProductController.edit);
@@ -16,6 +22,7 @@ routes.post('/products', multer.array('photos', 6), ProductController.post);
 routes.put('/products', multer.array('photos', 6), ProductController.put);
 routes.delete('/products', ProductController.delete);
 
+//Alias
 routes.get('/ads/create', function (req, res) {
   return res.redirect('/products/create');
 });
