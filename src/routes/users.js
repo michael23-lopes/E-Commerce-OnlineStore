5fe721f04763f042b8ed5acc73062dc9462d1e85
michael23-lopes <michael23.wellington@gmail.com>
2021-11-04 17:12:04 -0300

    Iniciando registro de usuarios

---
+++ src/routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const routes = express.Router();
+
+// const SessionController = require('../app/controllers/SessionController.js');
+const UserController = require('../app/controllers/UserController.js');
+
+// Login/Logout
+// routes.get('/login', SessionController.loginForm);
+// routes.get('/login', SessionController.login);
+// routes.get('/logout', SessionController.logout);
+
+// Reset password /forgot
+// routes.get('/forgot-password', SessionController.forgotPassword);
+// routes.get('/password-reset', SessionController.resetPassword);
+// routes.post('/forgot-password', SessionController.forgotPassword);
+// routes.post('/password-reset', SessionController.resetPassword);
+
+// user register UserController
+routes.get('/register', UserController.registerForm);
+// routes.post('/register', UserController.post);
+
+// routes.get('/', UserController.show);
+// routes.put('/', UserController.update);
+// routes.delete('/', UserController.delete);
+
+module.exports = routes;
